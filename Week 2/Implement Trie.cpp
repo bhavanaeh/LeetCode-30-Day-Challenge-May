d33d38a80b893b72197b05522907851729eaf755
Bhavana <bhavana_sundar@srmuniv.edu.in>
2020-05-14 22:41:09 +0530

    Create Implement Trie.cpp

---
+++ Week 2/Implement Trie.cpp
@@ -0,0 +1,86 @@
+/*Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.*/
+
+
+class Trie {
+public:
+    /** Initialize your data structure here. */
+    unordered_map<char,Trie*> children;
+    Trie() {
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+         Trie * node = this;
+        for(char letter : word)
+            {
+            if(node->children.find(letter)==node->children.end())
+            {
+                Trie *newNode = new Trie();
+                node->children[letter] = newNode;
+            }
+            node = node->children[letter];
+        }
+        node->children['*'] = NULL;
+        
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        Trie * node = this;
+        for(char letter:word)
+        {
+            if(node->children.find(letter)!=node->children.end())
+            {
+                node = node->children[letter];
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return node->children.find('*')!=node->children.end()?true:false;
+        
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        Trie * node = this;
+        for(char letter:prefix)
+        {
+            if(node->children.find(letter)!=node->children.end())
+            {
+                node = node->children[letter];
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+        
+    };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
